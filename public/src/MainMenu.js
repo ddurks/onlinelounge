a9ca1f6c1a815a47a0cf9d1347d00f557c819d30
abc <abc@example.com>
2021-12-24 11:42:36 -0600

    login bugfix

--- public/src/MainMenu.js
+++ public/src/MainMenu.js
@@ -8,36 +8,16 @@ export class MainMenu extends Phaser.Scene {
     create() {
         this.input.keyboard.on('keydown', this.handleKey, this);
 
-        var text = this.add.text(10, 10, 'Please login to play', { color: '#fbf236', fontFamily: 'gaming2', fontSize: '16px '});
+        this.text = this.add.text(10, 10, 'Please login to play', { color: '#fbf236', fontFamily: 'gaming2', fontSize: '16px '});
 
         var element = this.add.dom(OL.world.width/2, OL.world.height/2).createFromCache('nameform');
 
         element.setPerspective(800)
         element.addListener('click');
-
-        var thisScene = this;
     
-        element.on('click', function (event) {
-    
-            if (event.target.name === 'loginButton')
-            {
-                var inputUsername = this.getChildByName('username');
-                var inputPassword = this.getChildByName('password');
-    
-                if (inputUsername.value !== '')
-                {
-                    OL.username = inputUsername.value;
-                    OL.password = inputPassword.value;
-                    console.log(OL.username);
-                    this.removeListener('click');
-                    thisScene.clickStart(thisScene);
-    
-                    text.setText('Welcome ' + inputUsername.value);
-                }
-                else
-                {
-                    this.scene.tweens.add({ targets: text, alpha: 0.1, duration: 200, ease: 'Power3', yoyo: true });
-                }
+        element.on('click', (event) => {
+            if (event.target.name === 'loginButton') {
+                this.login();
             }
     
         });
@@ -49,14 +29,29 @@ export class MainMenu extends Phaser.Scene {
                 break;
             }
             case 'Enter': {
-                this.clickStart(this);
+                this.login();
                 break;
             }
             default: {}
         }
     }
 
-    clickStart(scene) {
+    login() {
+        var inputUsername = document.getElementById('username');
+        var inputPassword = document.getElementById('password');
+
+        if (inputUsername.value !== '') {
+            OL.username = inputUsername.value;
+            OL.password = inputPassword.value;
+            console.log(OL.username);
+            this.clickStart();
+        } else {
+            this.scene.tweens.add({ targets: text, alpha: 0.1, duration: 200, ease: 'Power3', yoyo: true });
+        }
+    }
+
+    clickStart() {
+        this.text.setText('Welcome ' + OL.username);
         this.scene.start('Controls');
         this.scene.start('DigitalPlanet', {
             spawn: {
