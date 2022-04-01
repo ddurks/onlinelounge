4da71dd42f8442f95abbe4a7f13d23451f539e76
dldurks <dldurks@gmail.com>
2022-04-01 10:51:06 -0500

    prod

---
+++ startprod.sh
@@ -0,0 +1,4 @@
+sudo killall node
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 3000
+nohup npm start &
