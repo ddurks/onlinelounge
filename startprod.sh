46ffb0ccf55968540db9d8cb8544edcb19bfdac9
dldurks <dldurks@gmail.com>
2022-04-01 11:15:19 -0500

    startprod update

--- startprod.sh
+++ startprod.sh
@@ -1,4 +1,7 @@
 sudo killall node
+cd public
+npx webpack
+cd ../
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 3000
 nohup npm start &
